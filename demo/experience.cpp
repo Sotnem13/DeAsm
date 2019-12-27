9f6d66dd56abc3df00e2211fb7440847a9766ad5
Sotnem13 <asabitov@avtodoria.ru>
2019-12-27 15:01:55 +0300

    other way to realize VM

--- demo/experience.cpp
+++ demo/experience.cpp
@@ -10,18 +10,55 @@
 
 struct {
 
+    int a;
+    int b;
+    int c;
 
+    bool running;
+    char* cur;
 
 } glob_context;
 
 
 
-using op = void (*)(void);
+using op = void (*)();
 
 
+
+void add(){
+    glob_context.c = glob_context.a + glob_context.b;
+}
+
+void geta() {
+    std::cin >> glob_context.a;
+}
+
+void getb() {
+    std::cin >> glob_context.b;
+}
+
+void stop() {
+    glob_context.running = false;
+}
+
+void print() {
+    std::cout << glob_context.c << std::endl;
+}
 
 int main() {
 
+
+    op ops[] = { stop, geta, getb, add, print };
+
+    glob_context.running = 1;
+
+    char code[] = {1,2,3,4,0};
+
+    auto ip = &code[0];
+
+    while (glob_context.running && *ip) {
+        ops[*ip++]();
+    }
 
 
 //    CPU c;
